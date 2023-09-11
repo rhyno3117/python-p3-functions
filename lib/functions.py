84c1ec11590f5a543f9e416dcbc118106e5b00da
Ryan Meyers <rhynogolfer42@gmail.com>
2023-09-11 13:44:38 -0600

    Completed

--- lib/functions.py
+++ lib/functions.py
@@ -4,10 +4,10 @@ def greet_programmer():
     print("Hello, programmer!")
 
 def greet(name):
-    print(f"Hello, ${name}!")
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    print(f"Hello, ${name}!")
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     return num1 + num2
